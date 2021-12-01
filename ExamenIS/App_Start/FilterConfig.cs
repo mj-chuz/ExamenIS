49a5c65f078529731fd7c1480a6340544ad7de59
mj-chuz <medranom778@gmail.com>
2021-12-01 01:58:19 -0600

    Implementación de Bootstrap 5 y parte de la página principal

---
+++ ExamenIS/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ExamenIS
+{
+  public class FilterConfig
+  {
+    public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+    {
+      filters.Add(new HandleErrorAttribute());
+    }
+  }
+}
