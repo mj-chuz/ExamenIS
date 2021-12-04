2694a245e4df528561dd06fc8575126d0d8bf252
mj-chuz <medranom778@gmail.com>
2021-12-03 22:03:14 -0600

    Codigo base para crear la pizza personalizada Falta corregir el diseño y el controlador

--- ExamenIS/Controllers/PizzaController.cs
+++ ExamenIS/Controllers/PizzaController.cs
@@ -1,4 +1,4 @@
-﻿ using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,19 +7,51 @@ using ExamenIS.Models;
 using ExamenIS.Handlers;
 namespace ExamenIS.Controllers
 {
-    public class PizzaController : Controller
+  public class PizzaController : Controller
+  {
+    // GET: Pizza
+    [HttpGet]
+    public ActionResult CrearPizza()
     {
-        // GET: Pizza
-        [HttpGet]
-        public ActionResult CrearPizza()
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult CrearPizza(PizzaModel pizzaCreada)
+    {
+      String pizza = "ingrediente";
+      List<String> ingredientes = new List<String>();
+      String ingredienteAgregado = "";
+      for (int ingrediente = 1; ingrediente < 10; ingrediente++)
+      {
+        ingredienteAgregado = Request.Form[pizza + Convert.ToString(ingrediente)];
+        if (ingredienteAgregado != null)
         {
-            return View();
+          ingredientes.Add(ingredienteAgregado);
         }
 
-        [HttpPost]
-        public ActionResult CrearPizza(PizzaModel pizzaCreada)
+      }
+      pizzaCreada.Ingredientes = ingredientes;
+      ViewBag.ExitoAlCrear = false;
+      try
+      {
+        if (ModelState.IsValid)
         {
-          return View();
+          ViewBag.ExitoAlCrear = true;
+          if (ViewBag.ExitoAlCrear)
+          {
+            ViewBag.Message = "La pizza fue creado con éxito :D";
+            ModelState.Clear();
+          }
         }
+        return View();
+      }
+      catch
+      {
+        ViewBag.Message = "Algo salió mal y no fue posible crear la pizza :(";
+
+        return View(); //si falla se regresa a la vista original pero sin el mensaje
+      }
+    }
   }
 }
--- ExamenIS/Models/PizzaModel.cs
+++ ExamenIS/Models/PizzaModel.cs
@@ -2,18 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+using System.ComponentModel.DataAnnotations;
 namespace ExamenIS.Models
 {
   public class PizzaModel
   {
     public String Nombre { get; set; }
+
     public int Disponibilidad { get; set; }
     public int Precio { get; set; }
+
+    [Required(ErrorMessage = "Es necesario que indique la cantidad de queso")]
+    [Display(Name = "Cantidad de queso*")]
     public String CantidadQueso { get; set; }
+
+    [Required(ErrorMessage = "Es necesario que indique el tipo de salsa")]
+    [Display(Name = "Tipo de salsa*")]
     public String TipoSalsa { get; set; }
+
+    [Required(ErrorMessage = "Es necesario que indique el tipo de masa")]
+    [Display(Name = "Tipo de masa*")]
     public String TipoMasa { get; set; }
+
+    [Required(ErrorMessage = "Es necesario que indique el tamaño de la pizza")]
+    [Display(Name = "Tamaño de la pizza*")]
     public String Tamano { get; set; }
+
     public List<String> Ingredientes { get; set; }
 
     
