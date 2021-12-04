8e80ac2f292984457ffdf72b13488f2f857d5493
mj-chuz <medranom778@gmail.com>
2021-12-04 16:40:21 -0600

    Se agregan los productos seleccionados al carrito

---
+++ ExamenIS/Scripts/AgregarProductos.js
@@ -0,0 +1,26 @@
+﻿var precioTotal = 0;
+var productosComprados = [];
+
+function agregarArticulo(nombre, precio) {
+    var nuevoProducto = document.createElement("li");
+    nuevoProducto.style = "margin-bottom:30px;";
+    var nombreArticulo = document.createTextNode(nombre + " ₡" + precio);
+    nuevoProducto.appendChild(nombreArticulo);
+    document.getElementById("listaArticulos").appendChild(nuevoProducto);
+    obtenerArticulos(nombre);
+    calcularTotal(precio);
+    console.log(nombre);
+
+}
+
+function calcularTotal(precio) {
+    precioTotal = parseFloat(precioTotal) + parseFloat(precio);
+    console.log(precioTotal);
+    document.getElementById("total-pagar").innerHTML = "Subtotal: " + precioTotal;
+}
+
+function obtenerArticulos(nombre) {
+    productosComprados.push(nombre);
+    console.log(productosComprados);
+    return productosComprados;
+}
